0b5160b8a9070826c312532a3bb305fa1fb1f461
Egor V. Kraevsky <mail4xor@mail.ru>
2021-11-29 22:25:35 +1000

    lesson 2

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,21 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const RootComponent = defineComponent({    
+    name: 'Root',
+
+    data() {
+        return {
+            counter: 0,
+        }
+    },
+
+    methods: {
+        incrementCounter() {
+            this.counter++;
+        },
+    }
+
+});
+
+const app = createApp(RootComponent);
+const vm = app.mount('#app');
